302ce412cae121b6546f7f9a0454df5d58e741e1
김용민 <dydals3440@gmail.com>
2023-07-03 13:33:16 +0900

    feat: MainPage / LoginPage / Structure

---
+++ main.js
@@ -0,0 +1,6 @@
+// 로그인 로직은 건들이시면 안됩니다.
+document.querySelector('.login-button').addEventListener('click', function () {
+  window.location.href = './LoginPage/login.html';
+});
+
+// 여기서부터 카카오 맵 로직 구현 부탁드립니다.
