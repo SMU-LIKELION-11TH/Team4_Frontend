4a004e342321712b4c32e40373f6f145ba0224f4
rladlsrua794 <84882961+rladlsrua794@users.noreply.github.com>
2023-07-10 22:37:41 +0900

    오류수정
    test

--- my.js
+++ my.js
@@ -1,17 +1,17 @@
 document.querySelector('.login-button').addEventListener('click', function () {
-    window.location.href = '../LoginPage/login.html';
-  });
-  
-  var imageInput = document.getElementById('image');
-  var profileImage = document.getElementById('profileimg');
+  window.location.href = '../LoginPage/login.html';
+});
 
-  imageInput.addEventListener('change', function (event) {
-    var file = event.target.files[0];
-    var reader = new FileReader();
-  
-    reader.onload = function (e) {
-    profileImage.src = e.target.result;
-  };
-  
-  reader.readAsDataURL(file);
-  });
+var imageInput = document.getElementById('image');
+var profileImage = document.getElementById('profileimg');
+
+imageInput.addEventListener('change', function (event) {
+  var file = event.target.files[0];
+  var reader = new FileReader();
+
+  reader.onload = function (e) {
+  profileImage.src = e.target.result;
+};
+
+reader.readAsDataURL(file);
+});
